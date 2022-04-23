3f4264815142c152eedc7c1287cbe115ab19d01d
takeyu1013 <yuto1013@gmail.com>
2022-04-23 21:35:01 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -6,6 +6,16 @@ import (
 	"time"
 )
 
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+// var big int = 9223372036854775807 + 1
+const big = 9223372036854775807 + 1
+
 var (
 	i    int     = 1
 	f64  float64 = 1.2
@@ -28,4 +38,43 @@ func main() {
 	fmt.Println(user.Current())
 	fmt.Println(i, f64, s, t, f)
 	foo()
+	fmt.Println(Pi, Username, Password)
+	fmt.Println(big - 1)
+	/*
+		var (
+			u8  uint8     = 255
+			i8  int8      = 127
+			f32 float32   = 0.2
+			c64 complex64 = -5 + 12i
+		)
+		fmt.Println(u8, i8, f32, c64)
+		fmt.Printf("type=%T value=%v\n", u8, u8)
+	*/
+
+	/*
+		fmt.Println("1 + 1 =", 1+1)
+		fmt.Println("10 - 1 =", 10-1)
+		fmt.Println("10 / 2 =", 10/2)
+		fmt.Println("10 / 3 =", 10/3)
+		fmt.Println("10.0 / 3 =", 10.0/3)
+		fmt.Println("10 / 3.0 =", 10/3.0)
+		fmt.Println("10 % 2 =", 10%2)
+		fmt.Println("10 % 3 =", 10%3)
+	*/
+
+	/*
+		x := 0
+		fmt.Println(x)
+		// x = x + 1
+		x++
+		fmt.Println(x)
+		// x = x - 1
+		x--
+		fmt.Println(x)
+	*/
+
+	fmt.Println(1 << 0) // 0001 0001
+	fmt.Println(1 << 1) // 0001 0010
+	fmt.Println(1 << 2) // 0001 0100
+	fmt.Println(1 << 3) // 0001 1000
 }
