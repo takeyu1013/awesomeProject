448e4fde42a6ebc3b14a1612013c4d4c0a4fe324
takeyu1013 <yuto1013@gmail.com>
2022-04-24 13:00:45 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/user"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -92,4 +93,68 @@ Test`)
 
 	fmt.Println("\"")
 	fmt.Println(`"`)
+	t, f := true, false
+	fmt.Printf("%T, %v %t\n", t, 1, t)
+	fmt.Printf("%T, %v %t\n", f, 0, f)
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(false && false)
+
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(false || false)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
+
+	var s1 string = "14"
+	i, _ := strconv.Atoi(s1)
+	fmt.Printf("%T %v\n", i, i)
+
+	h := "Hello World"
+	fmt.Println(string(h[0]))
+
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
+
+	/*
+		var b [2]int = [2]int{100, 200}
+		b = append(b, 300)
+		fmt.Println(b)
+	*/
+
+	var b []int = []int{100, 200}
+	b = append(b, 300)
+	fmt.Println(b)
+
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
+
+	n[2] = 100
+	fmt.Println(n)
+
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
 }
