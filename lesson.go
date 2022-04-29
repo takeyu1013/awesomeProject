838d9f15dc900d46790729bbc01936d1c7136468
takeyu1013 <yuto1013@gmail.com>
2022-04-29 15:20:07 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -157,4 +157,57 @@ Test`)
 	fmt.Println(board)
 	n = append(n, 100, 200, 300, 400)
 	fmt.Println(n)
+	n1 := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n1), cap(n1), n1)
+	n1 = append(n1, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n1), cap(n1), n1)
+	n1 = append(n1, 1, 2, 3, 4, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n1), cap(n1), n1)
+
+	a1 := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(a1), cap(a1), a1)
+
+	b1 := make([]int, 0)
+	var c []int
+	fmt.Printf("len=%d cap=%d value=%v\n", len(b1), cap(b1), b1)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 5)
+	c = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		c = append(c, i)
+		fmt.Println(c)
+	}
+	fmt.Println(c)
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
+
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	/*
+		var m3 map[string]int
+		m3["pc"] = 5000
+		fmt.Println(m3)
+	*/
+
+	var s2 []int
+	if s2 == nil {
+		fmt.Println("Nil")
+	}
 }
