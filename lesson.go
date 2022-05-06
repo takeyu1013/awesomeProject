2b9f0c877b536939d098597894766e8d666857d3
takeyu1013 <yuto1013@gmail.com>
2022-05-06 18:09:20 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -214,6 +214,32 @@ func do(i interface{}) {
 	}
 }
 
+type Person2 struct {
+	Name string
+	Age  int
+}
+
+func (p Person2) String() string {
+	return fmt.Sprintf("My name is %v.", p.Name)
+}
+
+type UserNotFound struct {
+	Username string
+}
+
+func (e *UserNotFound) Error() string {
+	return fmt.Sprintf("User not found: %v", e.Username)
+}
+
+func myFunc() error {
+	// Something wrong
+	ok := false
+	if ok {
+		return nil
+	}
+	return &UserNotFound{Username: "mike"}
+}
+
 func main() {
 	fmt.Println("Hello, world!", time.Now())
 	fmt.Println(user.Current())
@@ -757,4 +783,24 @@ Test`)
 	var i5 int = 10
 	ii := float64(10)
 	fmt.Println(i5, ii)
+
+	mike2 := Person2{"Mike", 22}
+	fmt.Println(mike2)
+
+	if err := myFunc(); err != nil {
+		fmt.Println(err)
+	}
+
+	r4 := strings.NewReader("Hello, Reader!")
+
+	b3 := make([]byte, 8)
+	for {
+		n, err := r4.Read(b3)
+		fmt.Printf("n = %v err = %v b3 = %v\n", n, err, b3)
+		fmt.Printf("b3[:n] = %q\n", b3[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+
 }
