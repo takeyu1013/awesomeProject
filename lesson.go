136186a66883349843aa1bb06706b9e2ab589fd3
takeyu1013 <yuto1013@gmail.com>
2022-05-09 21:10:52 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -8,6 +8,7 @@ import (
 	"os/user"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -250,6 +251,29 @@ func (vertex Vertex3) Plus() int {
 
 func (vertex Vertex3) String() string {
 	return fmt.Sprintf("X is %d! Y is %d!", vertex.X, vertex.Y)
+}
+
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
 }
 
 func main() {
@@ -818,4 +842,20 @@ Test`)
 	v7 := Vertex3{3, 4}
 	fmt.Println(v7.Plus())
 	fmt.Println(v7)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("world", &wg)
+	normal("hello")
+	// time.Sleep(2000 * time.Millisecond)
+	wg.Wait()
+
+	s6 := []int{1, 2, 3, 4, 5}
+	c5 := make(chan int) // 15 15
+	go goroutine1(s6, c5)
+	go goroutine1(s6, c5)
+	x4 := <-c5
+	fmt.Println(x4)
+	y3 := <-c5
+	fmt.Println(y3)
 }
