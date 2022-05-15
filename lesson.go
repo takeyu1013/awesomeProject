6d73e1aa66727472d9bdc2504bcc123f37bb39e0
takeyu1013 <yuto1013@gmail.com>
2022-05-15 10:53:07 +0900

    ✨

--- lesson.go
+++ lesson.go
@@ -277,6 +277,73 @@ func goroutine1(s []int, c chan int) {
 	close(c)
 }
 
+func producer(ch chan int, i int) {
+	// Something
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	for i := range ch {
+		func() {
+			defer wg.Done()
+			fmt.Println("process", i*1000)
+		}()
+	}
+	fmt.Println("################")
+}
+
+func producer2(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second chan int, third chan int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
+}
+
+func goroutine2(ch chan string) {
+	for {
+		ch <- "packet from 2"
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func goroutine3(ch chan int) {
+	for {
+		ch <- 100
+		time.Sleep(1 * time.Second)
+	}
+}
+
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
 func main() {
 	fmt.Println("Hello, world!", time.Now())
 	fmt.Println(user.Current())
@@ -868,4 +935,83 @@ Test`)
 	for c := range ch2 {
 		fmt.Println(c)
 	}
+
+	var wg2 sync.WaitGroup
+	ch3 := make(chan int)
+
+	// Producer
+	for i := 0; i < 10; i++ {
+		wg2.Add(1)
+		go producer(ch3, i)
+	}
+
+	// Consumer
+	go consumer(ch3, &wg2)
+	wg2.Wait()
+	close(ch3)
+	// time.Sleep(2 * time.Second)
+	fmt.Println("Done")
+
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go producer2(first)
+	go multi2(first, second)
+	go multi4(second, third)
+	for result2 := range third {
+		fmt.Println(result2)
+	}
+
+	/*
+		c6 := make(chan string)
+		c7 := make(chan int)
+		go goroutine2(c6)
+		go goroutine3(c7)
+
+		for {
+			select {
+			case msg1 := <-c6:
+				fmt.Println(msg1)
+			case msg2 := <-c7:
+				fmt.Println(msg2)
+			}
+		}
+	*/
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+
+OuterLoop2:
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			break OuterLoop2
+			// return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+	fmt.Println("################")
+
+	// c6 := make(map[string]int)
+	c6 := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c6["key"] += 1
+			c6.Inc("Key")
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c6["key"] += 1
+			c6.Inc("Key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c6, c6.Value("Key"))
 }
