e96033fc6eeef36a57a5ca25916f690d9accb6f5
takeyu1013 <yuto1013@gmail.com>
2022-05-16 22:40:49 +0900

    ✨

--- main.go
+++ main.go
@@ -1,34 +1,49 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 )
 
+type T struct{}
+
+type Person struct {
+	Name      string   `json:"name,omitempty"`
+	Age       int      `json:"age,omitempty"`
+	Nicknames []string `json:"nicknames,omitempty"`
+	T         *T       `json:"T,omitempty"`
+}
+
+// func (p *Person) UnmarshalJSON(b []byte) error {
+// 	type Person2 struct {
+// 		Name string
+// 	}
+// 	var p2 Person2
+// 	err := json.Unmarshal(b, &p2)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	p.Name = p2.Name + "!"
+// 	return err
+// }
+
+// func (p Person) MarshalJSON() ([]byte, error) {
+// 	v, err := json.Marshal(&struct {
+// 		Name string
+// 	}{
+// 		Name: "Mr." + p.Name,
+// 	})
+// 	return v, err
+// }
+
 func main() {
-	// resp, _ := http.Get("http://example.com")
-	// defer resp.Body.Close()
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println(string(body))
+	b := []byte(`{"name":"Mike","age":20,"nicknames":[]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
 
-	base, _ := url.Parse("http://example.com/")
-	reference, _ := url.Parse("/test?a=1&b=2")
-	endpoint := base.ResolveReference(reference).String()
-	fmt.Println(endpoint)
-	// req, _ := http.NewRequest("GET", endpoint, nil)
-	req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte("password")))
-	req.Header.Add("If-None-Match", `W/"wyzzy"`)
-	q := req.URL.Query()
-	q.Add("c", "3&%")
-	fmt.Println(q)
-	fmt.Println(q.Encode())
-	req.URL.RawQuery = q.Encode()
-
-	var client *http.Client = &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
 }
