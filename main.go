e01b3c6e325819a2646f626df2501ebd19f3c9d0
takeyu1013 <yuto1013@gmail.com>
2022-05-23 23:34:01 +0900

    ✨

--- main.go
+++ main.go
@@ -25,12 +25,14 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// /view/test
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
-	p1.save()
-
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
-
+	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
